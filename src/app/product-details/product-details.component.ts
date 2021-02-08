39bb3e738746d2fa47e4cb552029f13d1aabc5e4
Kyle Day <day00090@morris.umn.edu>
2021-02-08 10:22:04 -0600

    Modify ngOnIt Method in product-details.component.ts
    This modification allows for the website to use the product array to find the appropriate product details for each product (step 4).

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -13,7 +13,13 @@ export class ProductDetailsComponent implements OnInit {
     private route: ActivatedRoute,
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
